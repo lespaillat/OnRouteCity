9857a0c74f9098365fdd64aa77d4963e6360b066
lespaillat <lespaillatj01@educantabria.es>
2020-11-02 02:32:54 +0100

    se le agregaron algunos textos y el boton al main

--- app/src/main/java/com/onroutecity/ActivitySplash.java
+++ app/src/main/java/com/onroutecity/ActivitySplash.java
@@ -2,13 +2,27 @@ package com.onroutecity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.animation.ObjectAnimator;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.widget.ProgressBar;
 
 public class ActivitySplash extends AppCompatActivity {
-
+    ProgressBar splashProgress;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        splashProgress=findViewById(R.id.splashProgress);
+        ObjectAnimator.ofInt(splashProgress,"progress",100).start();
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+               Intent intent =new Intent(ActivitySplash.this,MainActivity.class);
+               startActivity(intent);
+               finish();
+            }
+        }, 500);
     }
 }
